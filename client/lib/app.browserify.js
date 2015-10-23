7bbbebc52d01dbb35696a2d96cd5ddf7c8c318fa
crussi <chrisrussi@gmail.com>
2015-10-23 15:08:41 -0700

    moved injectTapEventPlugin

--- client/lib/app.browserify.js
+++ client/lib/app.browserify.js
@@ -1,3 +1,4 @@
+injectTapEventPlugin = require("react-tap-event-plugin");
 mui = require('material-ui');
 injectTapEventPlugin = require('react-tap-event-plugin');
 Styles = require('material-ui/lib/styles');
