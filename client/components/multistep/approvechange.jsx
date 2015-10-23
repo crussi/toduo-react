67d8f7a07f5ce3bd85473180357cca7470aec55c
crussi <chrisrussi@gmail.com>
2015-10-23 14:25:39 -0700

    trying to fix commit issue

--- client/components/multistep/approvechange.jsx
+++ client/components/multistep/approvechange.jsx
@@ -1,8 +1,8 @@
 ApproveChange = React.createClass({
-    //
+    //hello world
     render(){
         return <div>
-            <p>ApproveChange</p>
+            <p>ApproveChange now</p>
         </div>
     }
 });
